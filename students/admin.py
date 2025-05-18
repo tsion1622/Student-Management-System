624bc4165ad38cfeda5e2edc679bdca95a9aad2d
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-05-18 22:46:42 +0300

    added enrollment

--- students/admin.py
+++ students/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Student
-
+from .models import Student, Course, Enrollment
 
 admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Enrollment)
