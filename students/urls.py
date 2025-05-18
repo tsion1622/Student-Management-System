c17b5f9b4cf4621bcaf563716692f35292fa68bd
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-05-18 22:53:14 +0300

    Added route for student detail view with enrollments

--- students/urls.py
+++ students/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('add/', views.add, name='add'),
     path('edit/<int:id>/', views.edit, name='edit'),
     path('delete/<int:id>/', views.delete, name='delete'),
+    path('student/<int:student_id>/', views.student_detail, name='student_detail'),
+    path('student/<int:student_id>/add-enrollment/', views.add_enrollment, name='add_enrollment'),
+    path('enrollment/<int:enrollment_id>/delete/', views.delete_enrollment, name='delete_enrollment'),
 
 ]
