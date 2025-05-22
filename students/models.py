ba95bcc7bbd31154a3d038e8f9a20ac003b283b4
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-05-22 13:37:29 +0300

    added course and grade

--- students/models.py
+++ students/models.py
@@ -1,29 +1,28 @@
 from django.db import models
 
 class Student(models.Model):
-    student_number = models.PositiveIntegerField()
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=100)
-    field_of_study = models.CharField(max_length=50)
+    student_number = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    field_of_study = models.CharField(max_length=100)
     gpa = models.FloatField()
 
     def __str__(self):
-        return f'student: {self.first_name} {self.last_name}'
+        return f"{self.first_name} {self.last_name}"
 
 
-class Course(models.Model):  # <-- Removed extra space here
+class Course(models.Model):
     name = models.CharField(max_length=100)
-    code = models.CharField(max_length=20)
+    code = models.CharField(max_length=10)
 
     def __str__(self):
-        return f'{self.code} - {self.name}'
+        return self.name
 
 
 class Enrollment(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='enrollments')
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="enrollments")
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    grade = models.CharField(max_length=5)
-
+    grade = models.CharField(max_length=2)
     def __str__(self):
-        return f'{self.student.first_name} in {self.course.name} ({self.grade})'
+        return f"{self.student} - {self.course}"
