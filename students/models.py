4f738900e50a6f093e7b2d6dede77236fa535db8
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-05-22 09:58:01 +0300

    Add models for Student, Course, and Enrollment with relationships and string representations

--- students/models.py
+++ students/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Student(models.Model):
     student_number = models.PositiveIntegerField()
     first_name = models.CharField(max_length=50)
