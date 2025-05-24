78f8b793e473249ea4835f4250aec3ff396dc74a
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-05-24 22:40:19 +0300

    added add_enrolment

--- students/views.py
+++ students/views.py
@@ -1,11 +1,9 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 from .models import Student, Course, Enrollment
-from .forms import EnrollmentForm
-from .models import Student
-from .forms import StudentForm
+from .forms import StudentForm, EnrollmentForm
+from .forms import CourseForm
 
 def index(request):
     return render(request, 'students/index.html', {
@@ -13,29 +11,13 @@ def index(request):
     })
 
 def view_student(request, id):
-    student = Student.objects.get(pk=id)
     return HttpResponseRedirect(reverse('index'))
 
 def add(request):
     if request.method == 'POST':
         form = StudentForm(request.POST)
         if form.is_valid():
-            new_student_number = form.cleaned_data['student_number']
-            new_first_name = form.cleaned_data['first_name']
-            new_last_name = form.cleaned_data['last_name']
-            new_email = form.cleaned_data['email']
-            new_field_of_study = form.cleaned_data['field_of_study']
-            new_gpa = form.cleaned_data['gpa']
-
-            new_student = Student(
-                student_number=new_student_number,
-                first_name=new_first_name,
-                last_name=new_last_name,
-                email=new_email,
-                field_of_study=new_field_of_study,
-                gpa=new_gpa
-            )
-            new_student.save()
+            form.save()
             return render(request, 'students/add.html', {
                 'form': StudentForm(),
                 'success': True
@@ -47,8 +29,8 @@ def add(request):
     })
 
 def edit(request, id):
+    student = get_object_or_404(Student, pk=id)
     if request.method == 'POST':
-        student = Student.objects.get(pk=id)
         form = StudentForm(request.POST, instance=student)
         if form.is_valid():
             form.save()
@@ -57,7 +39,6 @@ def edit(request, id):
                 'success': True
             })
     else:
-        student = Student.objects.get(pk=id)
         form = StudentForm(instance=student)
     return render(request, 'students/edit.html', {
         'form': form
@@ -65,13 +46,32 @@ def edit(request, id):
 
 def delete(request, id):
     if request.method == 'POST':
-        student = Student.objects.get(pk=id)
+        student = get_object_or_404(Student, pk=id)
         student.delete()
     return HttpResponseRedirect(reverse('index'))
 
+def admin_student_list(request):
+    students = Student.objects.all()
+    return render(request, 'students/admin_student_list.html', {
+        'students': students
+    })
+def course_list(request):
+    return render(request, 'students/course_list.html', {
+        'courses': Course.objects.all()
+    })
+
+def add_course(request):
+    if request.method == 'POST':
+        form = CourseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('course_list')  # You can redirect anywhere
+    else:
+        form = CourseForm()
+    return render(request, 'students/add_course.html', {'form': form})
 def student_detail(request, student_id):
     student = get_object_or_404(Student, id=student_id)
-    enrollments = student.enrollments.select_related('course')
+    enrollments = Enrollment.objects.filter(student=student).select_related('course')
     form = EnrollmentForm()
     return render(request, 'students/student_detail.html', {
         'student': student,
@@ -79,7 +79,7 @@ def student_detail(request, student_id):
         'form': form,
     })
 
- def add_enrollment(request, student_id):
+def add_enrollment(request, student_id):
     student = get_object_or_404(Student, id=student_id)
     if request.method == 'POST':
         form = EnrollmentForm(request.POST)
@@ -90,4 +90,11 @@ def student_detail(request, student_id):
             return redirect('student_detail', student_id=student.id)
     else:
         form = EnrollmentForm()
-    return render(request, 'students/add_enrollment.html', {'form': form, 'student': student})
+    return render(request, 'students/add_enrollment.html', {
+        'form': form,
+        'student': student
+    })
+def delete_enrollment(request, enrollment_id):
+    enrollment = get_object_or_404(Enrollment, id=enrollment_id)
+    enrollment.delete()
+    return redirect('enrollment_list')  # Replace with the name of your enrollment list view
