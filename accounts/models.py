7ed92b19e79556229b97d10039ad8f4bb4362c87
tsion1622 <tayu.16.enye@gmail.com>
2025-06-02 22:23:01 +0300

    CustomUser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
