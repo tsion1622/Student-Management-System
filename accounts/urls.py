cde55959c6fbb342055d586056cda36c89ecbc27
tsion1622 <tayu.16.enye@gmail.com>
2025-06-04 23:50:15 +0300

    ak: Integrate accounts and students apps, update serializers, views, and frontend

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import register_view, login_view, logout_view
 
 urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('accounts/', include('accounts.urls')),
 ]
