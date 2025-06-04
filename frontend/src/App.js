152cb5a40fe26d955b5079ed1ef48b8fdc32cb93
tsion1622 <tayu.16.enye@gmail.com>
2025-06-04 23:50:53 +0300

    ak: Update accounts and students app URLs, views, and serializers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import TeacherDashboard from './components/TeacherDashboard';
+import StudentDashboard from './components/StudentDashboard';
+import ProtectedRoute from './components/ProtectedRoute';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/teacher" element={
+          <ProtectedRoute><TeacherDashboard /></ProtectedRoute>
+        } />
+        <Route path="/student" element={
+          <ProtectedRoute><StudentDashboard /></ProtectedRoute>
+        } />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
