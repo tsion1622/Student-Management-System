aa6ca09d6d731f809fce53384aec971d63b75e5b
tsion1622 <tayu.16.enye@gmail.com>
2025-06-06 21:54:30 +0300

    Add URL routing for custom user account APIs

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
-    path('accounts/', include('accounts.urls')),
 ]
