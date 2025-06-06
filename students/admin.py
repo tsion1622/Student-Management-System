c5276993d5dcf0a38289dabec19802da93b99abb
tsion1622 <tayu.16.enye@gmail.com>
2025-06-06 21:53:23 +0300

    Register Student model in Django admin

--- students/admin.py
+++ students/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Student
-from .models import Student, Course, Enrollment
+from .models import CustomUser, Student, Course, Enrollment, Attendance
 
+admin.site.register(CustomUser)
 admin.site.register(Student)
 admin.site.register(Course)
 admin.site.register(Enrollment)
+admin.site.register(Attendance)
