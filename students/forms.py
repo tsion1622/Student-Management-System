3a6e99cda14a025b71da71296ff215df64b42d07
tsion1622 <tayu.16.enye@gmail.com>
2025-06-06 21:53:46 +0300

    Create and configure StudentForm for data entry

--- students/forms.py
+++ students/forms.py
@@ -6,25 +6,37 @@ from .models import Attendance, Student
 
 
 class StudentForm(forms.ModelForm):
+    # Add related user fields manually
+    first_name = forms.CharField(max_length=150)
+    last_name = forms.CharField(max_length=150)
+    email = forms.EmailField()
+
     class Meta:
         model = Student
-        fields = ['student_number', 'first_name', 'last_name', 'email', 'field_of_study', 'gpa']
-        labels = {
-            'student_number': 'Student Number',
-            'first_name': 'First Name',
-            'last_name': 'Last Name',
-            'email': 'Email',
-            'field_of_study': 'Field of Study',
-            'gpa': 'GPA'
-        }
-        widgets = {
-            'student_number': forms.NumberInput(attrs={'class': 'form-control'}),
-            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'field_of_study': forms.TextInput(attrs={'class': 'form-control'}),
-            'gpa': forms.NumberInput(attrs={'class': 'form-control'}),
-        }
+        fields = ['student_number', 'field_of_study', 'gpa']  # Removed first_name, last_name, email here
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and self.instance.user:
+            self.fields['first_name'].initial = self.instance.user.first_name
+            self.fields['last_name'].initial = self.instance.user.last_name
+            self.fields['email'].initial = self.instance.user.email
+
+    def save(self, commit=True):
+        student = super().save(commit=False)
+        user = student.user
+
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+            student.save()
+        else:
+            # Optionally handle delayed save
+            pass
+        return student
 
     def clean_gpa(self):
         gpa = self.cleaned_data.get('gpa')
@@ -32,20 +44,22 @@ class StudentForm(forms.ModelForm):
             raise forms.ValidationError("GPA must be between 0.0 and 4.0")
         return gpa
 
-
 class EnrollmentForm(forms.ModelForm):
     class Meta:
         model = Enrollment
         fields = ['course', 'grade']
+
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
         fields = ['name', 'code']  # Add other fields if needed
+
 class GradeEntryForm(forms.ModelForm):
     class Meta:
         model = Enrollment
         fields = ['student', 'course', 'grade']        
+
 class AttendanceForm(forms.ModelForm):
     class Meta:
         model = Attendance
-        fields = ['student', 'date', 'status']        
+        fields = ['student', 'date', 'status']
